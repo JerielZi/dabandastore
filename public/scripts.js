baac6f973cb7f365a5a0a990001ceb81c4ba718c
JerielZi <jeriel_zinga@hotmail.com>
2020-05-10 21:06:02 +0100

    Formating the currency for the price

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,20 @@
 
+const Mask = {
+  apply(input, func) {
+    setTimeout(function() {
+      input.value = Mask[func](input.value)
+    }, 1)
+
+  },
+  formatAKZ(value) {
+    
+    value = value.replace(/\D/g,"")
+
+    return value = new Intl.NumberFormat('pt-AO', {
+      style: 'currency', 
+      currency: 'AKZ'
+    }).format(value/100)
+    
+    e.target.value = value
+  }
+}
