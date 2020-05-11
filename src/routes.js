bfd4ebacc9da19308407c56a73b1ce19050558b0
JerielZi <jeriel_zinga@hotmail.com>
2020-05-11 01:07:14 +0100

    Configuring some features

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,11 @@ routes.get('/', function(req, res) {
 })
 
 routes.get('/products/create', ProductController.create )
+routes.get('/products/:id/edit', ProductController.edit)
+
 routes.post('/products', ProductController.post)
+routes.put('/products', ProductController.put)
+routes.delete('/products', ProductController.delete)
 
 //Alias
 routes.get('/adds/create', function(req, res) {
